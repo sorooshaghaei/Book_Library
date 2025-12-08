7885365a9cb6d60dc93726b775d66712ebb12e30
Soroosh <soroosh77aghaei@gmail.com>
2025-12-08 22:42:12 +0100

    member class init

--- src/Member.py
+++ src/Member.py
@@ -1,2 +1,8 @@
 class Member:
-    pass
+    def __init__(self, name , id, borrowed_books):
+        self.name=name
+        self.id=id
+        self.borrowed_books=borrowed_books
+
+    def __str__(self):
+        pass
