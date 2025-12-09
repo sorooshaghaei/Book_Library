964aa6aca1e6ed1c8adb5b2ce30a4585c15cd07f
Soroosh <soroosh77aghaei@gmail.com>
2025-12-09 08:32:34 +0100

    member class

--- src/Member.py
+++ src/Member.py
@@ -1,8 +1,15 @@
 class Member:
-    def __init__(self, name , id, borrowed_books):
-        self.name=name
-        self.id=id
-        self.borrowed_books=borrowed_books
+    def __init__(self, name, id, borrowed_books):
+        self.name = name
+        self.id = id
+        self.borrowed_books = borrowed_books
 
     def __str__(self):
-        pass
+        print(
+            f"member name is {self.name} with id: {self.id} and borrowed books are: {self.borrowed_books}"
+        )
+
+
+if __name__ == "__main__":
+    member1 = Member("ali", 1, 0)
+    member1.__str__()
