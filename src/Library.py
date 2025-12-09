e7aad11e10b65fe451b0ca3894884d1dd292ff2e
Soroosh <soroosh77aghaei@gmail.com>
2025-12-09 08:37:45 +0100

    class library init

---
+++ src/Library.py
@@ -0,0 +1,24 @@
+class Library:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def add_book(self):
+        pass
+
+    def remove_book(self):
+        pass
+
+    def list_books(self):
+        pass
+
+    def add_member(self):
+        pass
+
+    def remove_member(self):
+        pass
+
+    def list_members(self):
+        pass
